23c671ae49921f5168c5fec365acd48aed86685d
Dong <ddliuhb@gmail.com>
2012-09-26 16:05:52 +0800

    test

---
+++ test/view.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @copyright Dong <ddliuhb@gmail.com>
+ * @licence http://maxmars.net/license/MIT
+ */
+require dirname(__FILE__).'/../view.php';
+
+class ViewTest extends PHPUnit_Framework_TestCase{
+}
