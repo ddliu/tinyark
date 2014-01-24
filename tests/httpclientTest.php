227e947ccaf08cd087b8f723c49af4a3595b7e7f
Dong <ddliuhb@gmail.com>
2014-01-24 15:03:45 +0800

    use httpbin.org to test httpclient

--- tests/httpclientTest.php
+++ tests/httpclientTest.php
@@ -29,26 +29,23 @@ class HttpClientTest extends PHPUnit_Framework_TestCase{
     public function testRequest()
     {
         $client = new ArkHttpClient();
-        $response = $client->get('http://php.net');
+        $response = $client->get('http://httpbin.org/get');
         $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($response->getInfo('url'), 'http://php.net');
-        $this->assertRegExp('/\>downloads\<\/a\>/i', $response->getContent());
+        $this->assertEquals($response->getInfo('url'), 'http://httpbin.org/get');
+        $this->assertContains('"Host": "httpbin.org"', $response->getContent());
 
-        $response = $client->get('http://www.yahoo.com/notexist');
+        $response = $client->get('http://httpbin.org/status/404');
         $this->assertEquals($response->getStatusCode(), 404);
 
         $this->assertEquals($response->hasError(), true);
 
         $this->assertEquals($response->hasError(array(200, 404)), false);
 
-        $response = $client->get('http://a.com');
-        $this->assertEquals($response->hasError(), true);
-
-        // $ua = 'Tinyark HTTP Client';
-        // $response = $client->get('http://whatsmyuseragent.com', null, array(
-        //     'User-Agent' => $ua,
-        // ));
-        // $this->assertContains($ua, $response->getContent());
+        $ua = 'Tinyark HTTP Client';
+        $response = $client->get('http://httpbin.org/user-agent', null, array(
+            'User-Agent' => $ua,
+        ));
+        $this->assertContains($ua, $response->getContent());
     }
 
 }
